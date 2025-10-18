91884f8c6841a5e05ca7dcb6cc8e8fa412ec8ec0
Avto Bagaturia <Avtobagaturia21@gmail.com>
2025-10-18 20:46:54 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+function sent() {
+    let btn1 = document.querySelector('.btn1');
+    btn1.addEventListener('click', function() {
+        alert('Message sent succesfully')
+        console.log('Message sent succesfully');
+        
+    })
+}
