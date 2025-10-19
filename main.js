79ec312ee6fda2f98c7a222f671e774669eb85f6
Avto Bagaturia <Avtobagaturia21@gmail.com>
2025-10-19 18:16:22 +0400

    Merged with remote repository

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 
+let btn1 = document.querySelector('.btn1');
+
+
+function inputValid() {
+  let inp1 = document.getElementById('inp1')
+  let inp2 = document.getElementById('inp2')
+  let inp3 = document.getElementById('inp3')
+  let inp4 = document.getElementById('inp4')
+
+  if(inp1 === " " || inp2 === " " || inp3 === " " || inp4 === " ") {
+    btn1.addEventListener('click', function() {
+        alert('Please fill all value')
+    })
+  }
+}
+
 
 function sent() {
-    let btn1 = document.querySelector('.btn1');
     btn1.addEventListener('click', function() {
         alert('Message sent succesfully')
         console.log('Message sent succesfully');
